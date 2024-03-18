117cc6961c2b40103e861591be6e69ae417de051
Brian Hall <brian.hall4@gmail.com>
2024-03-18 00:01:24 -0400

    mongo connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func newPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Main website",
+	})
+}
+
 func main() {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		fmt.Println("Failed to connect to MongoDB:", err)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		fmt.Println("Failed to ping MongoDB:", err)
+		return
+	}
+	fmt.Println("Connected to MongoDB!")
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-		})
-	})
+	router.GET("/index", newPage)
 	router.Run(":42069")
 }

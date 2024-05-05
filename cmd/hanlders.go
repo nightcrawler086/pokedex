29ede73263cdfc5090eee1b4af7addb14b16a0ce
Brian Hall <brian.hall4@gmail.com>
2024-05-04 22:09:46 -0400

    here's a table

--- cmd/hanlders.go
+++ cmd/hanlders.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"io"
 	"net/http"
 )
 
+const pokemonUri = "http://pokeapi.co/api/v2/pokemon"
+
 var tmpl *template.Template
+
+type Response struct {
+	Name    string    `json:"name"`
+	Results []Pokemon `json:"results"`
+}
+
+type Pokemon struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
 
 func init() {
 	if tmpl == nil {
@@ -13,17 +27,25 @@ func init() {
 	}
 }
 
-var i int
-
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-	i++
-	heading := "Hello"
-	if i%2 == 0 {
-		heading = "Bonjour"
+	response, err := http.Get(pokemonUri + "?limit=20")
+	if err != nil {
+		panic(err)
 	}
-	data := map[string]string{
-		"title":   "Golang Web Server",
-		"Heading": heading,
+	defer r.Body.Close()
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	var responseObj Response
+	err = json.Unmarshal(responseData, &responseObj)
+	if err != nil {
+		panic(err)
+	}
+	data := map[string]any{
+		"title":         "Pokedex",
+		"Heading":       "Pokedex",
+		"queryResponse": responseObj,
 	}
 	tmpl.ExecuteTemplate(w, "index.html", data)
 }
